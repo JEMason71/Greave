6a89c806e0a381178a4a6f96ad10fbd3e9e320df
Raine 'Gravecat' Simmons <cat@moxx.net>
2021-06-24 08:19:00 +0100

    Room descriptions are no longer shown if the room is too dark.

--- src/actions/look.cpp
+++ src/actions/look.cpp
@@ -17,40 +17,44 @@ void ActionLook::look(std::shared_ptr<Mobile> mob)
     const std::shared_ptr<Room> room = core()->world()->get_room(mob->location());
 
     const bool can_see = (room->light() >= Room::LIGHT_VISIBLE);
-    if (can_see) room->set_tag(RoomTag::Explored);
+    if (!can_see)
+    {
+        core()->message("{U}Darkness");
+        core()->message("{0}```{u}It is {B}pitch black{u}, and you can see {B}nothing{u}. You are likely to be eaten by a grue.");
+        core()->message("{0}{u}It's so {B}dark{u}, you can't see where the exits are!");
+        return;
+    }
+
+    room->set_tag(RoomTag::Explored);
 
     // Room name and description.
     core()->message("{G}" + room->name());
     core()->message("{0}```" + room->desc());
 
     // Obvious exits.
-    if (!can_see) core()->message("{0}{u}It's so {B}dark{u}, you can't see where the exits are!");
-    else
+    std::vector<std::string> exits_vec;
+    for (unsigned int e = 0; e < Room::ROOM_LINKS_MAX; e++)
     {
-        std::vector<std::string> exits_vec;
-        for (unsigned int e = 0; e < Room::ROOM_LINKS_MAX; e++)
+        std::string exit_name;
+        uint32_t room_link = room->link(e);
+        if (!room_link) continue;
+        if (room->link_tag(e, LinkTag::Hidden)) continue;   // Never list hidden exits.
+        exit_name = "{c}" + StrX::dir_to_name(e);
+        
+        if (room_link != Room::FALSE_ROOM)
         {
-            std::string exit_name;
-            uint32_t room_link = room->link(e);
-            if (!room_link) continue;
-            if (room->link_tag(e, LinkTag::Hidden)) continue;   // Never list hidden exits.
-            exit_name = "{c}" + StrX::dir_to_name(e);
-            
-            if (room_link != Room::FALSE_ROOM)
+            const std::shared_ptr<Room> link_room = core()->world()->get_room(room_link);
+            if (link_room->tag(RoomTag::Explored) && !link_room->tag(RoomTag::Maze)) exit_name += " {B}(" + link_room->name(true) + "){c}";
+            if (room->link_tag(e, LinkTag::Locked)) exit_name += " {u}[locked]{c}";
+            else if (room->link_tag(e, LinkTag::Openable))
             {
-                const std::shared_ptr<Room> link_room = core()->world()->get_room(room_link);
-                if (link_room->tag(RoomTag::Explored) && !link_room->tag(RoomTag::Maze)) exit_name += " {B}(" + link_room->name(true) + "){c}";
-                if (room->link_tag(e, LinkTag::Locked)) exit_name += " {u}[locked]{c}";
-                else if (room->link_tag(e, LinkTag::Openable))
-                {
-                    if (room->link_tag(e, LinkTag::Open)) exit_name += " {u}[open]{c}";
-                    else exit_name += " {u}[closed]{c}";
-                }
+                if (room->link_tag(e, LinkTag::Open)) exit_name += " {u}[open]{c}";
+                else exit_name += " {u}[closed]{c}";
             }
-
-            exits_vec.push_back(exit_name);
         }
 
-        if (exits_vec.size()) core()->message("{0}{g}Obvious exits: " + StrX::comma_list(exits_vec, StrX::CL_FLAG_USE_AND));
+        exits_vec.push_back(exit_name);
     }
+
+    if (exits_vec.size()) core()->message("{0}{g}Obvious exits: " + StrX::comma_list(exits_vec, StrX::CL_FLAG_USE_AND));
 }
