JEMason71/Greave

commit a77c6a8926eb5468441dd2914aa051bb49c3e5b6

Fixed bug #14 (The player's passage of time from actions is interrupted by taking damage)

src/world/time-weather.cpp
@@ -201,8 +201,9 @@ TimeWeather::LunarPhase TimeWeather::moon_phase() const
 }
 
 // Causes time to pass.
-bool TimeWeather::pass_time(float seconds)
+bool TimeWeather::pass_time(float seconds, bool interruptable)
 {
+    if (seconds > UNINTERRUPTABLE_TIME) interruptable = false;
     const std::shared_ptr<Player> player = core()->world()->player();
     const std::shared_ptr<Room> room = core()->world()->get_room(player->location());
     const bool indoors = room->tag(RoomTag::Indoors);
@@ -220,15 +221,15 @@ bool TimeWeather::pass_time(float seconds)
     int old_hp = player->hp();
     while (seconds_to_add--)
     {
-        int hp = player->hp();
         if (player->is_dead()) return false;    // Don't pass time if the player is dead.
 
-        // Wake the player if they are resting, and take damage.
-        if (hp < old_hp)
+        // Interrupt the action if the player takes damage.
+        if (interruptable)
         {
-            if (seconds > UNINTERRUPTABLE_TIME) return false;
+            const int hp = player->hp();
+            if (hp < old_hp) return false;
+            old_hp = hp;
         }
-        old_hp = hp;
 
         m_time_passed++;    // The total time passed in the game. This will loop every 136 years, but that's not a problem; see time_passed().
 

src/actions/combat/combat.cpp
@@ -77,7 +77,7 @@ void Combat::change_stance(std::shared_ptr<Mobile> mob, CombatStance stance)
     if (mob->is_player()) core()->message("{W}You assume " + stance_str + "{W}.");
     else if (mob->location() == core()->world()->player()->location() && core()->world()->get_room(mob->location())->light(core()->world()->player()) >= Room::LIGHT_VISIBLE)
         core()->message("{W}" + mob->name(Mobile::NAME_FLAG_THE | Mobile::NAME_FLAG_CAPITALIZE_FIRST) + " {W}assumes " + stance_str + "{W}!");
-    mob->pass_time(STANCE_CHANGE_TIME);
+    mob->pass_time(STANCE_CHANGE_TIME, false);
 }
 
 // Generates a standard-format damage number string.

src/actions/combat/melee.cpp
@@ -75,7 +75,7 @@ bool Melee::attack(std::shared_ptr<Mobile> attacker, std::shared_ptr<Mobile> def
         perform_attack(attacker, defender, EquipSlot::HAND_OFF, wield_type[0], wield_type[1]);
         attacked = true;
     }
-    attacker->pass_time(attack_speed);
+    attacker->pass_time(attack_speed, false);
     return attacked;
 }
 

src/world/mobile.cpp
@@ -335,13 +335,13 @@ float Mobile::parry_mod() const
 uint16_t Mobile::parser_id() const { return m_parser_id; }
 
 // Causes time to pass for this Mobile.
-bool Mobile::pass_time(float seconds)
+bool Mobile::pass_time(float seconds, bool interruptable)
 {
     // For the player, time passes in the world itself.
     if (is_player())
     {
         if (!seconds) core()->guru()->nonfatal("Attempt to pass 0 seconds on player character.", Guru::WARN);
-        return core()->world()->time_weather()->pass_time(seconds);
+        return core()->world()->time_weather()->pass_time(seconds, interruptable);
     }
 
     // For NPCs, any action clears their action timer.

src/world/mobile.hpp
@@ -101,7 +101,7 @@ public:
     void                new_parser_id();                            // Generates a new parser ID for this Mobile.
     float               parry_mod() const;                          // Returns the modified chance to parry for this Mobile, based on equipped gear.
     uint16_t            parser_id() const;                          // Retrieves the current ID of this Mobile, for parser differentiation.
-    bool                pass_time(float seconds = 0.0f);            // Causes time to pass for this Mobile.
+    bool                pass_time(float seconds = 0.0f, bool interruptable = true); // Causes time to pass for this Mobile.
     void                reduce_hp(int amount);                      // Reduces this Mobile's hit points.
     int                 restore_hp(int amount);                     // Restores a specified amount of hit points.
     virtual uint32_t    save(std::shared_ptr<SQLite::Database> save_db);    // Saves this Mobile.

src/world/time-weather.hpp
@@ -32,7 +32,7 @@ public:
     void        load(std::shared_ptr<SQLite::Database> save_db);    // Loads the time/weather data from disk.
     std::string month_name() const;                 // Returns the name of the current month.
     LunarPhase  moon_phase() const;                 // Gets the current lunar phase.
-    bool        pass_time(float seconds);           // Causes time to pass.
+    bool        pass_time(float seconds, bool interruptable);       // Causes time to pass.
     void        save(std::shared_ptr<SQLite::Database> save_db) const;  // Saves the time/weather data to disk.
     std::string season_str(Season season) const;    // Converts a season enum to a string.
     TimeOfDay   time_of_day(bool fine) const;       // Returns the current time of day (morning, day, dusk, night).

commit 329249162d403dd9ec87a9e2d7142da1f74cac1b

Fixed a bug caused by a typo in commit a77c6a8.

src/world/time-weather.cpp
@@ -203,7 +203,7 @@ TimeWeather::LunarPhase TimeWeather::moon_phase() const
 // Causes time to pass.
 bool TimeWeather::pass_time(float seconds, bool interruptable)
 {
-    if (seconds > UNINTERRUPTABLE_TIME) interruptable = false;
+    if (seconds <= UNINTERRUPTABLE_TIME) interruptable = false;
     const std::shared_ptr<Player> player = core()->world()->player();
     const std::shared_ptr<Room> room = core()->world()->get_room(player->location());
     const bool indoors = room->tag(RoomTag::Indoors);